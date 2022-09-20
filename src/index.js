740ae5f23e8dcf63c4087277036c78c4fdcec20f
Vika <mironenko>
2022-09-20 21:34:11 +0300

    add home task

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ const add123 = (a, b) => a + b;
 
 const res = add123(222, 3);
 console.log("Helloasda");
+
+let unreadUserMessages = 44;
+let isRegistered = true;
+let userFavoriteGenre = 'Horor';
+
+
+// let a = '#';
+// while (a.length < 8) {
+//     console.log (a);
+//     a = a + '#';   
+    
+// }
+
+let a = '#';
+let line = ' ' + a + ' ' + a + ' ' + a + ' ' + a + ' ' + a;
+console.log(line);
+let line2 = 
+
+
+
+
+    
+
+
+

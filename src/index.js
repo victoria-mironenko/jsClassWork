dd003899aab46f12596a8d5bcf814e8fdc558eb5
Vika <mironenko>
2022-09-23 21:30:14 +0300

    function work

--- src/index.js
+++ src/index.js
@@ -2,30 +2,57 @@ import "./main.scss";
 
 const add123 = (a, b) => a + b;
 
-const res = add123(222, 3);
-console.log("Helloasda");
+// const res = add123(222, 3);
+// console.log("Helloasda");
 
-let unreadUserMessages = 44;
-let isRegistered = true;
-let userFavoriteGenre = 'Horor';
+// const currency = {
+//     usd: 'dollar',
+//     blr: 'bel rouble',
+//     rus: 'russian rouble'
 
-
-// let a = '#';
-// while (a.length < 8) {
-//     console.log (a);
-//     a = a + '#';   
-    
 // }
+// let currencyType = prompt('Введите вашу валюту');
+// console.log(currency[currencyType]);
 
-let a = '#';
-let line = ' ' + a + ' ' + a + ' ' + a + ' ' + a + ' ' + a;
-console.log(line);
-let line2 = 
+// let descriptor = Object.defineProperty(currency, 'usd',{
+//     configurable: false,
+//     emumerable: false,
+// })
+// console.log(currency);
 
 
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+// let sum = 0;
+// for(let key in salaries){
+//     sum += salaries[key]
+//     console.log(sum)
+// }
+// console.log(sum)
+
+// function Sum(){
+//     for (const arg of arguments) {
+//         console.log(arg);
+//     }
+// }
+// Sum('Ann','sen','bit');
 
 
+//  const number = (...param) => {
+//      let num = 0;
+//      for (const plus of param) {
+//          num += plus;
+        
+//      }
+//      return num;
+//  }   
+//  const res = number(1,2,3,4,5,6)
+
+//  console.log(res)
+
+ const min = (a, b) =>  (a < b)? a: b;
     
-
-
-
+console.log(min(22,100));
